32304c7c28f35babd8ea3a1e03711926b78d4814
Paweł Mroziński <IZ-PIB\pawel.mrozinski@srvgeno01.izoo.krakow.pl>
2022-01-18 10:08:37 +0100

    test

---
+++ init/init.py
@@ -0,0 +1,6 @@
+files=[ "/home/IZ-PIB/barbara.kulesza/numerical.EBV/Step2_202112/EBVstdInfo.cows.SCS.202112",
+	"/home/IZ-PIB/barbara.kulesza/numerical.EBV/Step2_202112/EBVstdInfo.cows.MFP.202112" ,
+	"/home/IZ-PIB/barbara.kulesza/numerical.EBV/Step2_202112/EBVstdInfo.bulls.MLP.202112",
+	"/home/IZ-PIB/barbara.kulesza/numerical.EBV/Step2_202112/EBVstdInfo.cows.MLP.202112",
+	"/home/IZ-PIB/barbara.kulesza/numerical.EBV/Step2_202112/EBVstdInfo.bulls.SCS.202112",
+	"/home/IZ-PIB/barbara.kulesza/numerical.EBV/Step2_202112/EBVstdInfo.bulls.MFP.202112" ]
