988452d694e8954a63b8d5bbd76c24f69eeeb264
Pawel Mrozinski <pawel@szachowe.pl>
2022-01-26 11:12:36 +0100

    dasdsd

--- scr/preProcessing.py
+++ scr/preProcessing.py
@@ -38,9 +38,9 @@ def toSmallDf3(df):
 	dfTemp['fullNumber']=df.iloc[:,0]
 	for line, row in enumerate(dfTemp.itertuples(),1):
 		if row.fullNumber:
-			dfTemp.at[row.index,'sex']=row.fullNumber
-			dfTemp.at[row.index,'country']=row.fullNumber
-			dfTemp.at[row.index,'id']=row.fullNumber
+			dfTemp.at[row.index,'sex']=1
+			dfTemp.at[row.index,'country']=1
+			dfTemp.at[row.index,'id']=1
 	end = time.time()
 	duration = end-start
 	logs.logTime(toSmallDf3.__name__, duration)
