0e787bbf87ba1a6bb371768b27b67b259b7ec317
Pawel Mrozinski <pawel@szachowe.pl>
2022-01-28 10:51:55 +0100

    asdasds

---
+++ scr/analytics.py
@@ -0,0 +1,9 @@
+import dask.dataframe as ddf
+import pandas as pd
+
+# Funkcja przyjmije jako argumenty: Ramkę  Danych Pandas oraz nazwę kolumny
+# w której będzie szukać duplikatów
+# zwraca ramkę danych zawierającą powtórzone wiersze 
+
+def checkForDups(df, col):
+    return df[df.duplicated(subset=[col], keep=False)]
