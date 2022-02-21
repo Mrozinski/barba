db53ae75d9053ddd44cf3cbc98651d7013debac1
Pawel Mrozinski <pawel@szachowe.pl>
2022-02-21 13:24:11 +0100

    asds

--- scr/analytics.py
+++ scr/analytics.py
@@ -2,7 +2,7 @@ import dask.dataframe as ddf
 import pandas as pd
 
 def checkForDups(df, col):
-    return df[df.duplicated(subset=[col], keep=True)]
+    return df[df.duplicated(subset=[col], keep=False)]
 
 def compare2df(df1, df2, col):
     return df1.drop(df1.join(df2.set_index(col).index))
