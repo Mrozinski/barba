219fb95291ad5c873549707420517fbdfc28af70
Pawel Mrozinski <pawel@szachowe.pl>
2022-02-21 13:21:18 +0100

    keep=True

--- scr/analytics.py
+++ scr/analytics.py
@@ -2,7 +2,7 @@ import dask.dataframe as ddf
 import pandas as pd
 
 def checkForDups(df, col):
-    return df[df.duplicated(subset=[col], keep=False)]
+    return df[df.duplicated(subset=[col], keep=True)]
 
 def compare2df(df1, df2, col):
     return df1.drop(df1.join(df2.set_index(col).index))
