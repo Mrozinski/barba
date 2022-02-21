3f2b7b9c510c8c01dffc703889a17cc2df208502
Pawel Mrozinski <pawel@szachowe.pl>
2022-02-21 07:07:53 +0100

    Poprawki

--- main.py
+++ main.py
@@ -6,23 +6,20 @@ import scr.logs as logs
 import scr.preProcessing as pre
 import scr.readFile as rf
 
-def dupFullNumber(df):
-	return(df[df.duplicated(subset=['fullNumber'])])
+def main():
+	logs.onStart()
+	for fileName in files.files:
+		df=rf.readFile3(fileName)
+		if len(df.index) == 0:
+			print('error')
+		else:
+			print(fileName)
+			print(df.head())
+			print(pre.toSmallDf(df).head())
+			#print(pre.toSmallDf2(df).head())
+			#print(pre.toSmallDf3(df).head())
+			#print(pre.toSmallDf3(df).head())
+	logs.onExit()
+	return 0
 
-logs.onStart()
-
-for fileName in files.files:
-	df=rf.readFile3(fileName)
-	df2=rf.readFile(fileName)
-	if len(df.index) == 0:
-		print('error')
-	else:
-		print(fileName)
-		print(df.head())
-		print(df2.head())
-		#print(pre.toSmallDf(df).head())
-		#print(pre.toSmallDf2(df).head())
-		#print(pre.toSmallDf3(df).head())
-		#print(pre.toSmallDf3(df).head())
-
-logs.onExit()
+main()
--- scr/analytics.py
+++ scr/analytics.py
@@ -7,3 +7,6 @@ import pandas as pd
 
 def checkForDups(df, col):
     return df[df.duplicated(subset=[col], keep=False)]
+
+def compare2df(df1, df2, col):
+    return df1.drop(df1.join(df2.set_index(col).index))
